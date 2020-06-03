dd9d2598c6e1168bb528a68170898931953418b2
Rachel Williams <rachelwilliams659@gmail.com>
2020-06-02 20:07:27 -0600

    Add keydownHandler function and refactor code to be consistent with style guide

--- main.js
+++ main.js
@@ -1,40 +1,45 @@
 
 var newActivitySection = document.querySelector('.new-activities-section');
+var timeInput = document.querySelector('.time');
 var minutesInput = document.getElementById('minutes');
 var secondsInput = document.getElementById('seconds');
 var accomplishmentInput = document.getElementById('accomplishment');
-var startTimerBtn = document.querySelector(".timer-button");
+var startTimerBtn = document.querySelector('.timer-button');
 var timer = document.querySelector('.timer');
 var newActivityHeading = document.querySelector('.new-activity');
 var activityForm = document.querySelector('.form');
 var currentActivityHeading = document.querySelector('.current-activity');
-var createNewActivBtn = document.querySelector(".create-new-activity");
-var logBtn = document.querySelector(".log-btn");
+var createNewActivBtn = document.querySelector('.create-new-activity');
+var logBtn = document.querySelector('.log-btn');
 var activityData = [];
 var category = '';
 
-//Consider refactoring the below into 1 event listener by adding a common class to the minutes & seconds input fields
-minutesInput.addEventListener('keydown', formNumberValidation);
-secondsInput.addEventListener('keydown', formNumberValidation);
+timeInput.addEventListener('keydown', formNumberValidation);
 newActivitySection.addEventListener('click', clickHandler);
+
+function keydownHandler(event) {
+  if (event.target.classList.contains('minutes') || event.target.classList.contains('seconds')) {
+    formNumberValidation(event);
+  };
+}
 
 function clickHandler(event) {
   if (event.target.classList.contains('form-button')) {
     changeBtnColor(event);
-  }
+  };
   if (event.target.classList.contains('start-btn')) {
     validateForm();
-  }
-  if (event.target.classList.contains("timer-button")) {
+  };
+  if (event.target.classList.contains('timer-button')) {
     var currentActivity = activityData[activityData.length - 1];
     currentActivity.startTimer();
-  }
-  if (event.target.classList.contains("log-btn")) {
+  };
+  if (event.target.classList.contains('log-btn')) {
     logActivity();
-  }
-  if (event.target.classList.contains("create-new-activity")) {
+  };
+  if (event.target.classList.contains('create-new-activity')) {
     returnToForm();
-  }
+  };
 }
 
 function displayTimerInput(activity) {
@@ -58,118 +63,111 @@ function replaceTimerDisplay(activity) {
   var minutesToDisplay = activity.minutes;
   if (secondsToDisplay < 10) {
     secondsToDisplay = '0' + secondsToDisplay;
-  }
+  };
   if (minutesToDisplay < 10) {
     minutesToDisplay = '0' + minutesToDisplay;
-  }
+  };
   timerDisplay.innerText = `${minutesToDisplay}:${secondsToDisplay}`;
   descriptionDisplay.innerText = activity.description;
 }
 
 function changeOutlineColor() {
   var timerBtn = document.querySelector('.timer-button');
-  if (category === "Study") {
-    timerBtn.classList.add('start-study-outline')
-  }
-  if (category === "Meditate") {
-    timerBtn.classList.add('start-meditate-outline')
-  }
-  if (category === "Exercise") {
-    timerBtn.classList.add('start-exercise-outline')
-  }
+  if (category === 'Study') {
+    timerBtn.classList.add('start-study-outline');
+  };
+  if (category === 'Meditate') {
+    timerBtn.classList.add('start-meditate-outline');
+  };
+  if (category === 'Exercise') {
+    timerBtn.classList.add('start-exercise-outline');
+  };
 }
 
 function validateForm() {
   event.preventDefault();
-  startTimerBtn.innerText = "START ACTIVITY";
+  startTimerBtn.innerText = 'START ACTIVITY';
   changeOutlineColor();
-  logBtn.classList.add("hidden");
-  var categoryError = document.querySelector(".category-error");
-  var accomplishmentError = document.querySelector(".accomplishment-error");
-  var minutesError = document.querySelector(".minutes-error");
-  var secondsError = document.querySelector(".seconds-error");
+  logBtn.classList.add('hidden');
+  var categoryError = document.querySelector('.category-error');
+  var accomplishmentError = document.querySelector('.accomplishment-error');
+  var minutesError = document.querySelector('.minutes-error');
+  var secondsError = document.querySelector('.seconds-error');
   categoryError.innerHTML = '';
   accomplishmentError.innerHTML = '';
   minutesError.innerHTML = '';
   secondsError.innerHTML = '';
-  if (category !== '' && accomplishmentInput.value !== "" && minutesInput.value !== "" && secondsInput.value !== "" && secondsInput.value < 60) {
+  if (category !== '' && accomplishmentInput.value !== '' && minutesInput.value !== '' && secondsInput.value !== '' && secondsInput.value < 60) {
     createActivityInstance();
   } else {
-      if (category === '') {
-        categoryError.innerHTML = `
-            <p><img src="assets/warning.svg" class="warning-image"/> A category is required.</p>
-        `;
-      }
-      if (accomplishmentInput.value === "") {
-        accomplishmentError.innerHTML = `
-            <p><img src="assets/warning.svg" class="warning-image"/> A description is required.</p>
-        `;
-      }
-      if (minutesInput.value === "") {
-        minutesError.innerHTML = `
-          <p><img src="assets/warning.svg" class="warning-image"/> A time is required.</p>
-        `;
-      }
-      if (secondsInput.value === "" || secondsInput.value >= 60) {
-        secondsError.innerHTML = `
-          <p><img src="assets/warning.svg" class="warning-image"/> A valid time is required.</p>
-        `;
-      }
-    }
-  }
+    if (category === '') {
+      categoryError.innerHTML = `
+          <p><img src="assets/warning.svg" class="warning-image"/> A category is required.</p>
+      `;
+    };
+    if (accomplishmentInput.value === '') {
+      accomplishmentError.innerHTML = `
+          <p><img src="assets/warning.svg" class="warning-image"/> A description is required.</p>
+      `;
+    };
+    if (minutesInput.value === '') {
+      minutesError.innerHTML = `
+        <p><img src="assets/warning.svg" class="warning-image"/> A time is required.</p>
+      `;
+    };
+    if (secondsInput.value === '' || secondsInput.value >= 60) {
+      secondsError.innerHTML = `
+        <p><img src="assets/warning.svg" class="warning-image"/> A valid time is required.</p>
+      `;
+    };
+  };
+}
 
 function changeBtnColor(event) {
-  categoryArray = ["study", "meditate", "exercise"];
+  categoryArray = ['study', 'meditate', 'exercise'];
   for (var i = 0; i < categoryArray.length; i++) {
     var currentButton = document.querySelector(`.${categoryArray[i]}-btn`);
     var currentIcon = document.querySelector(`.${categoryArray[i]}-passive`);
-    currentButton.classList.remove(`${categoryArray[i]}-btn-active`)
-    currentIcon.src = `assets/${categoryArray[i]}.svg`
+    currentButton.classList.remove(`${categoryArray[i]}-btn-active`);
+    currentIcon.src = `assets/${categoryArray[i]}.svg`;
     if (event.target.classList.contains(`${categoryArray[i]}-btn`)) {
       event.target.classList.add(`${categoryArray[i]}-btn-active`);
       currentIcon.src = `assets/${categoryArray[i]}-active.svg`;
-    }
+    };
     category = event.target.value;
-  }
+  };
 }
 
 function createActivityInstance(event) {
-  var userDescription = document.getElementById("accomplishment").value;
-  var userMinutes = parseInt(document.getElementById("minutes").value);
-  var userSeconds = parseInt(document.getElementById("seconds").value);
+  var userDescription = document.getElementById('accomplishment').value;
+  var userMinutes = parseInt(document.getElementById('minutes').value);
+  var userSeconds = parseInt(document.getElementById('seconds').value);
   var activity = new Activity(category, userDescription, userMinutes, userSeconds);
   activityData.push(activity);
   activityForm.reset();
   displayTimerInput(activity);
 }
 
-function formNumberValidation(event) {
-  if (event.key === "e" || event.key === "+" || event.key === "." || event.key === "-") {
-    event.preventDefault();
-  };
-}
-
 function logActivity() {
-  var defaultWords = document.querySelector(".default-words");
-  var loggedActivitiesSection = document.querySelector(".logged-activities");
-  loggedActivitiesSection.innerHTML = "";
-  defaultWords.classList.add("hidden");
+  var defaultWords = document.querySelector('.default-words');
+  var loggedActivitiesSection = document.querySelector('.logged-activities');
+  loggedActivitiesSection.innerHTML = '';
+  defaultWords.classList.add('hidden');
   for (var i = 0; i < activityData.length; i++) {
-    // if (activityData[i].category === "Study") {}
-  var newLoggedActivity = `
-  <section class="card-type">
-      <section class="card">
-        <section class="selected-type">${activityData[i].category}</section>
-        <section class="selected-time">${activityData[i].minutes} MINUTES ${activityData[i].seconds} SECONDS</section>
-        <section class="selected-activity">${activityData[i].description}</section>
+    var newLoggedActivity = `
+    <section class="card-type">
+        <section class="card">
+          <section class="selected-type">${activityData[i].category}</section>
+          <section class="selected-time">${activityData[i].minutes} MINUTES ${activityData[i].seconds} SECONDS</section>
+          <section class="selected-activity">${activityData[i].description}</section>
+        </section>
+        <p class="border-line-${activityData[i].category}"></p>
       </section>
-      <p class="border-line-${activityData[i].category}"></p>
-    </section>
-  `;
-  loggedActivitiesSection.insertAdjacentHTML("afterbegin", newLoggedActivity);
-  timer.classList.add("hidden");
-  createNewActivBtn.classList.remove("hidden");
-  }
+    `;
+    loggedActivitiesSection.insertAdjacentHTML('afterbegin', newLoggedActivity);
+    timer.classList.add('hidden');
+    createNewActivBtn.classList.remove('hidden');
+  };
 }
 
 function returnToForm() {
@@ -177,11 +175,11 @@ function returnToForm() {
   activityForm.classList.remove('hidden');
   currentActivityHeading.classList.add('hidden');
   timer.classList.add('hidden');
-  createNewActivBtn.classList.add("hidden");
+  createNewActivBtn.classList.add('hidden');
 }
 
 function formNumberValidation(event) {
-  var charactersToExclude = ['e', 'E', '+', '.', '-']
+  var charactersToExclude = ['e', 'E', '+', '.', '-'];
   if (charactersToExclude.includes(event.key)) {
     event.preventDefault();
   };
